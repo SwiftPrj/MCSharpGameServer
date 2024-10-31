dabd5cbf4c4136b3f7216b4461af9d50b8704dd5
nullify <63555452+nullifiedpntr@users.noreply.github.com>
2024-10-31 21:51:19 +0200

    Update Objects.cs

--- LegacyServer/Objects.cs
+++ LegacyServer/Objects.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +8,7 @@ namespace LegacyServer
 {
     public static class Objects
     {
-        public static string[] objects = { "par_solid", "obj_barrel" };
+        public static string[] objects = { "par_solid", "obj_barrel", "obj_projectile" };
 
         public static string[] GetObjects()
         {
